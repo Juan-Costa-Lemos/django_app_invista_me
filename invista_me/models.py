ef23894924e8aa58436d36fbf51cb319e9f05b84
Juan-Costa-Lemos <juancostask@gmail.com>
2022-08-30 20:58:26 -0300

    correção de um bug na exclusão

--- invista_me/models.py
+++ invista_me/models.py
@@ -7,5 +7,5 @@ class Investimento(models.Model):
     investimento = models.TextField(max_length=255)
     valor = models.FloatField()
     pago = models.BooleanField(default=False )
-    data = models.DateField(datetime.now)
+    data = models.DateField()
     
